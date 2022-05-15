2ad74fb12db59253d5ab34957757518477ffc56e
bert2 <shuairan@gmail.com>
2022-05-15 15:14:55 +0200

    only respond when client sent something

--- src/Server.cs
+++ src/Server.cs
@@ -13,8 +13,12 @@ var buf = new byte[1024];
 
 while (client.Connected) {
     var n = await client.ReceiveAsync(buf, SocketFlags.None);
-    Console.WriteLine($"Received {n} bytes: {UTF8.GetString(buf, index: 0, count: n)}");
 
-    _ = await client.SendAsync(UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
-    Console.WriteLine("Sent PONG");
+    if (n > 0) {
+        Console.WriteLine($"Received {n} bytes: {UTF8.GetString(buf, index: 0, count: n)}");
+        _ = await client.SendAsync(UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
+        Console.WriteLine("Sent PONG");
+    }
+
+    await Task.Delay(100);
 }
