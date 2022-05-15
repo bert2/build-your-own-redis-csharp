48c693c8d9a478950e4a7cad28f86a5c9df05d6e
bert2 <shuairan@gmail.com>
2022-05-15 15:11:10 +0200

    keep responsding with PONG

--- src/Server.cs
+++ src/Server.cs
@@ -1,9 +1,20 @@
-﻿using System.Net;
-using System.Net.Sockets;
+﻿using System.Net.Sockets;
 
 using static System.Text.Encoding;
 
-var server = new TcpListener(IPAddress.Any, 6379);
+var server = TcpListener.Create(port: 6379);
 server.Start();
-var client = server.AcceptSocket();
-await client.SendAsync(UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
+Console.WriteLine("Listening...");
+
+var client = await server.AcceptSocketAsync();
+Console.WriteLine("Client connected");
+
+var buf = new byte[1024];
+
+while (client.Connected) {
+    var n = await client.ReceiveAsync(buf, SocketFlags.None);
+    Console.WriteLine($"Received {n} bytes: {UTF8.GetString(buf, index: 0, count: n)}");
+
+    _ = await client.SendAsync(UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);
+    Console.WriteLine("Sent PONG");
+}
